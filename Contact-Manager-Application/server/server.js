5c99363f122b5d063c0293b8a521c0d10ac1c4c7
Atamjeetsingh1 <160144768+Atamjeetsingh1@users.noreply.github.com>
2024-07-15 03:05:07 +0530

    Add files via upload

---
+++ Contact-Manager-Application/server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const connectdb = require("./config/dbConnection");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+
+connectdb();
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+var whitelist = [
+  "http://localhost:3000",
+  "https://contact-manager-mern.netlify.app",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
