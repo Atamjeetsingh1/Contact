331e1097ef893062be1d1910b42105c75340a9cf
Atamjeetsingh1 <160144768+Atamjeetsingh1@users.noreply.github.com>
2024-07-15 02:58:47 +0530

    Delete Contact- directory

--- Contact-/server/models/Contact.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const contactSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  avatar: String
-});
-
-module.exports = mongoose.model('Contact', contactSchema);
